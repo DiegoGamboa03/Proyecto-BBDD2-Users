87d99759e5669917b9bc4d05f68045083bbd8481
Diego <diego.gamboaa03@gmail.com>
2022-07-30 17:19:20 -0400

    Iniciando proyecto

---
+++ src/Routes/users.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const router = new Router();
+const conn = require('D:/Proyecto BBDD2/proyecto-BBDD2/src/Config/DatabaseConfig');
+
+router.get('/AnualSalaryChanges', (req, res) => {
+    const sql = 'CALL totalCambiosSalariosAnual();';
+
+    conn.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+    });
+});
+
+module.exports = router;
